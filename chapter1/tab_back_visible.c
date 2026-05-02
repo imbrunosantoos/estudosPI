58735bf0f9fe087cdd82fd8d424f79cf1edb44f0
imbrunosantoos <brunoribeirosouzasantos@gmail.com>
2026-05-02 14:38:32 +0100

    feat: function that make the tab, backspace and backslash visible

---
+++ chapter1/tab_back_visible.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//program that makes tabs and backspaces visible
+
+int main(){
+    int c;
+    while((c = getchar()) != EOF){
+        if (c == '\t'){
+            putchar('\\');
+            putchar('t');
+        }
+        else if(c == '\b'){
+            putchar('\\');
+            putchar('b');
+        }
+        else if(c == '\\'){
+            putchar('\\');
+            putchar('\\');
+        }
+        else {
+            putchar(c);
+        }
+
+    }
+    return 0;
+}
