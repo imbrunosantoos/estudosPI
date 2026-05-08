e3ca119824474c1dfde985582e1f2ecb0363e426
imbrunosantoos <brunoribeirosouzasantos@gmail.com>
2026-05-08 21:23:05 +0100

    feat: add remove function

--- q2/ex14.c
+++ q2/ex14.c
@@ -22,3 +22,24 @@ void appendL(LInt *l, int new){
         temp ->prox = novo;
     }
 }
+int remove(LInt *l, int x){
+    LInt anterior =  NULL;
+    LInt novo = *l;
+    
+    while(novo != NULL && novo->valor != x){
+        anterior = novo;
+        novo = novo->prox;
+
+    }
+    if (novo == NULL){
+        return 1;
+    }
+    else if(anterior == NULL){
+        *l=novo->prox;
+    }else{
+        anterior->prox= novo->prox;
+    }
+    free(novo);
+    return 0;
+
+}
